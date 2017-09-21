8506ea64356ada242bd1af73be28cd95f09673cd
lgdcnm <18268815524@163.com>
2017-09-21 11:07:17 +0800

    a

--- js/yh.js
+++ js/yh.js
@@ -1,6 +1,21 @@
-$(function() {
+$(function(){
 	$(".topnav ul li").click(function() {
 		$(".topnav ul li").removeClass("active")
 		$(this).addClass("active");
 	});
+	
+	
+	
+	//回到顶部
+	
+	$(".totop").click(function(){
+		$("body").animate({
+			"scrollTop":0
+		},300);
+	});
+	
 });
+
+
+
+
